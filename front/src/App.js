30a5331ada18a6ac5b58fbedcf82d3987274b4b9
neilroutley <lenderachu@gmail.com>
2019-02-21 15:46:23 -0800

    react frontend added

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render(){
+    console.log("Rendering");
+    return (<div className="App">Hi Neil!</div>);
+  }
+}
+
+export default App;
