416bbeb44fc1d0f5ed16a95b4eb45a870dd285ea
neilroutley <lenderachu@gmail.com>
2019-02-21 16:51:09 -0800

    react build served by express

--- front/src/App.js
+++ front/src/App.js
@@ -3,9 +3,23 @@ import React, {Component} from "react";
 import "./App.css";
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch("/api/getMessage")
+      .then( res => console.log("got data", res));
+  }
+
   render(){
     console.log("Rendering");
-    return (<div className="App">Hi Neil!</div>);
+    return (
+      <div className="App">
+        <h1>Comments!</h1>
+        <div id="comments"></div>
+        <form action="/api/createMessage">
+          <input type="text" id="comment"/>
+        </form>
+        <div>Made by Neil</div>
+      </div>);
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/createMessage', function(req, res, next) {
+  res.render("Hi!");
+});
+
+router.get('/getMessage', function(req, res, next) {
+	console.log("getMessages!!");
+  res.send([
+  		{text:"Neil"}
+  	]);
 });
 
 module.exports = router;
